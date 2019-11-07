a2af4689131c04875525fa82d7b939507c917716
Kiernan Nicholls <kiernann@protonmail.com>
2019-11-07 11:04:14 -0500

    Save life tibble

---
+++ data-raw/life.R
@@ -0,0 +1,27 @@
+## code to prepare `life` dataset goes here
+library(tidyverse)
+library(rvest)
+
+# List of U.S. states and territories by area
+life <-
+  read_html("https://w.wiki/BeA") %>%
+  html_node(".wikitable") %>%
+  html_table(fill = TRUE, header = FALSE) %>%
+  na_if("") %>%
+  slice(-(1)) %>%
+  as_tibble() %>%
+  filter(!is.na(X1)) %>%
+  select(
+    state = X2,
+    life = X3
+  ) %>%
+  inner_join(states, by = "state") %>%
+  select(fips, life) %>%
+  arrange(desc(life))
+
+write_csv(
+  x = life,
+  path = "data-raw/life.csv"
+)
+
+usethis::use_data(life, overwrite = TRUE)
