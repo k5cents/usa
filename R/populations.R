c27372471c857aa4b57e9197210b78ed987a1987
Kiernan Nicholls <kiernann@protonmail.com>
2019-11-06 19:13:20 -0500

    Rename data.R to populations.R

--- R/populations.R
+++ R/populations.R
@@ -6,14 +6,11 @@
 #'
 #' @format A tibble with 52 rows and 7 variables:
 #' \describe{
-#'   \item{sumlev}{Geographic summary level}
-#'   \item{region}{Census Region code}
-#'   \item{division}{Census Division code}
 #'   \item{fips}{State FIPS code}
 #'   \item{state}{State name}
-#'   \item{pop}{Resident population estimate (2018-07-01)}
+#'   \item{population}{Resident population estimate (2018-07-01)}
 #'   \item{adult}{Percent of resident population age 18 years and over (2018-07-01)}
 #'   ...
 #' }
 #' @source \url{https://www2.census.gov/programs-surveys/popest/datasets/2010-2018/state/detail/}
-"pop"
+"populations"
