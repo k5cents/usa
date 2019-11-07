7dd752cca74bd7d203abdb642b6c74f7657968d7
Kiernan Nicholls <kiernann@protonmail.com>
2019-11-06 19:23:32 -0500

    Update tests to rename populations

--- tests/testthat/test-populations.R
+++ tests/testthat/test-populations.R
@@ -2,6 +2,6 @@ library(testthat)
 library(state2)
 
 test_that("Population data has 52 rows of 7 variables", {
-  expect_equal(nrow(pop), 52)
-  expect_equal(ncol(pop), 7)
+  expect_equal(nrow(populations), 52)
+  expect_equal(ncol(populations), 4)
 })
