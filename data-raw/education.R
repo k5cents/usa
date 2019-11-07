834cc050f92d2fbe29b9272248f22ea2fae38573
Kiernan Nicholls <kiernann@protonmail.com>
2019-11-07 10:38:24 -0500

    Save education tibble

---
+++ data-raw/education.R
@@ -0,0 +1,32 @@
+## code to prepare `education` dataset goes here
+library(tidyverse)
+library(magrittr)
+library(rvest)
+
+# List of U.S. states and territories by area
+education <-
+  read_html("https://w.wiki/Bd$") %>%
+  html_node(".wikitable") %>%
+  html_table(fill = TRUE, header = FALSE) %>%
+  slice(-(1:2)) %>%
+  as_tibble() %>%
+  select(
+    state = X1,
+    high = X2,
+    bach = X4,
+    grad = X6
+  ) %>%
+  mutate_at(
+    .vars = c(2:4),
+    .funs = function(x) parse_number(x)/100
+  ) %>%
+  inner_join(states, by = "state") %>%
+  select(fips, high, bach, grad) %>%
+  arrange(desc(high))
+
+write_csv(
+  x = education,
+  path = "data-raw/education.csv"
+)
+
+usethis::use_data(education, overwrite = TRUE)
