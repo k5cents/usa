73269f2b51ca01da500b5d65460983fd459016e9
Kiernan Nicholls <kiernann@protonmail.com>
2019-12-12 11:17:42 -0500

    Get area from tiger/census

--- data-raw/area.R
+++ data-raw/area.R
@@ -1,29 +1,57 @@
 ## code to prepare `area` dataset goes here
 library(tidyverse)
-library(magrittr)
 library(rvest)
 
-# List of U.S. states and territories by area
-areas <-
-  read_html("https://w.wiki/Bdw") %>%
-  html_node(".wikitable") %>%
-  html_table(fill = TRUE, header = FALSE) %>%
-  slice(-(1:2), -c(59:62)) %>%
-  as_tibble() %>%
-  select(
-    name = X1,
-    area = X3,
-    land = X8
-  ) %>%
-  mutate(
-    area = parse_number(area) %>% round(2),
-    land = parse_number(land) %>% divide_by(100) %>% round(4)
-  ) %>%
-  inner_join(state_names) %>%
-  select(fips, area, land) %>%
-  arrange(desc(area))
+# an area measurement providing the size, in square meters, of the land portions
+# of geographic entities for which the Census Bureau tabulates and disseminates
+# data.
 
-write_csv(
-  x = areas,
-  path = "data-raw/areas.csv"
-)
+# Area is calculated from the specific boundary recorded for each entity in the
+# Census Bureau's geographic database (see "MAF/TIGER Database"). The Census
+# Bureau provides area measurement data for both land area and water area...
+
+# Land area measurements are originally recorded as whole square meters (to
+# convert square meters to square kilometers, divide by 1,000,000; to convert
+# square kilometers to square miles, divide by 2.58999; to convert square meters
+# to square miles, divide by 2,589,988).
+
+source <- "https://tigerweb.geo.census.gov/tigerwebmain/Files/acs19/tigerweb_acs19_state_us.html"
+tiger <- read_html(source)
+html_text(html_node(tiger, "title"))
+# U.S. States - Current/ACS19 - Data as of January 1, 2019
+html_node(tiger, "table") %>% html_attr("summary")
+# This table containing the U.S. States - Current/ACS19 - Data as of January 1,
+# 2019, gives the data (headers) for each States record (rows). For the record
+# layout, please reference back to the Record Layout link on the TIGERweb
+# State-Based Tabular Data Files page for the given record type.
+area <- tiger %>%
+  html_node("table") %>%
+  html_table() %>%
+  as_tibble(.name_repair = "unique") %>%
+  mutate(
+    STATE = str_pad(STATE, 2, pad = "0"),
+    sqmi = round(AREALAND/2589988, 2),
+    CENTLAT = round(CENTLAT, 4),
+    CENTLON = round(CENTLON, 4)
+  ) %>%
+  select(
+    state = STUSAB,
+    sqmi,
+    lat = CENTLAT,
+    long = CENTLON,
+  )
+
+# save as rda and csv
+usethis::use_data(area, overwrite = TRUE)
+write_csv(area, "data-raw/area.csv")
+
+# overwrite datasets::state.area
+state.area <- area$sqmi
+usethis::use_data(state.area, overwrite = TRUE)
+write_lines(state.area, "data-raw/state.area.csv")
+
+## code to prepare `centers` dataset goes here
+state.center <- list(x = area$long, y = area$lat)
+usethis::use_data(state.center, overwrite = TRUE)
+# overwrite datasets::state.center
+write_lines(state.center, "data-raw/state.center.csv")
