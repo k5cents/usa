5e512f0379dd23317d44a3309bb50f354bed9ef4
Kiernan Nicholls <kiernann@protonmail.com>
2020-05-13 12:38:59 -0400

    Ignore case in convert

--- R/state-convert.R
+++ R/state-convert.R
@@ -3,7 +3,7 @@
 #' Take a vector of state identifiers and convert to a common format.
 #'
 #' @param x A character vector of: state names, abbreviations, or FIPS codes.
-#' @param to The format returned: name, abbreviation, or FIPS codes.
+#' @param to The format returned: "abb", "name" or "fips".
 #' @return A character vector of single format state identifiers.
 #' @examples
 #' state_convert(c("AL", "Vermont", "06"))
@@ -11,18 +11,26 @@
 state_convert <- function(x, to = NULL) {
   to <- match.arg(to, c("abb", "names", "fips"), several.ok = FALSE)
   abbs <- grep("^[A-Z]{2}$", x)
-  full <- grep("^[A-z]{3,}$", x)
+  full <- grep("^[a-zA-Z ]{3,}$", x)
   fips <- grep("^\\d+$", x)
   x[fips] <- sprintf("%02d", as.numeric(x[fips]))
+  match2 <- function(x, table) {
+    x <- tolower(x)
+    table <- tolower(table)
+    match(x, table)
+  }
   if (to == "abb") {
-    x[full] <- usa::state.abb[match(x[full], usa::state.name)]
-    x[fips] <- usa::state.abb[match(x[fips], usa::states$fips)]
+    x[abbs] <- usa::state.abb[match2(x[abbs], usa::state.abb)]
+    x[full] <- usa::state.abb[match2(x[full], usa::state.name)]
+    x[fips] <- usa::state.abb[match2(x[fips], usa::states$fips)]
   } else if (to == "names") {
-    x[abbs] <- usa::state.name[match(x[abbs], usa::state.abb)]
-    x[fips] <- usa::state.name[match(x[fips], usa::states$fips)]
+    x[abbs] <- usa::state.name[match2(x[abbs], usa::state.abb)]
+    x[full] <- usa::state.name[match2(x[full], usa::state.name)]
+    x[fips] <- usa::state.name[match2(x[fips], usa::states$fips)]
   } else if (to == "fips") {
-    x[abbs] <- usa::states$fips[match(x[abbs], usa::state.abb)]
-    x[full] <- usa::states$fips[match(x[full], usa::state.name)]
+    x[abbs] <- usa::states$fips[match2(x[abbs], usa::state.abb)]
+    x[full] <- usa::states$fips[match2(x[full], usa::state.name)]
+    x[fips] <- usa::states$fips[match2(x[fips], usa::states$fips)]
   }
   return(x)
 }
--- tests/testthat/test-convert.R
+++ tests/testthat/test-convert.R
@@ -1,15 +1,15 @@
 library(testthat)
 library(usa)
 
-x <- c("ID", "Vermont", 6)
+x <- c("ID", "new mexico", 2)
 test_that("conversion to abbreviation", {
-  expect_equal(state_convert(x, "abb"), c("ID", "VT", "CA"))
+  expect_equal(state_convert(x, "abb"), c("ID", "NM", "AK"))
 })
 
 test_that("conversion to abbreviation", {
-  expect_equal(state_convert(x, "fips"), c("16", "50", "06"))
+  expect_equal(state_convert(x, "fips"), c("16", "35", "02"))
 })
 
 test_that("conversion to full name", {
-  expect_equal(state_convert(x, "names"), c("Idaho", "Vermont", "California"))
+  expect_equal(state_convert(x, "names"), c("Idaho", "New Mexico", "Alaska"))
 })
